f23af573fd1707754cc3b19c9eff56c9c29bb4e7
DrParadox <80583203+DrParadox1090@users.noreply.github.com>
2023-05-12 23:53:48 +0530

    Offset Update
    Updated Offsets

--- FXAS21002C_Basic.h
+++ FXAS21002C_Basic.h
@@ -1,14 +1,13 @@
 
 #ifndef FXAS21002C_BASIC_H
 #define FXAS21002C_BASIC_H
-//#include "I2C_device.h"
 #include "i2cdevice.h"
 
 class FXAS21002CBasic: protected i2cdevice
 {
 protected:
    float sensitivity;
-   float gyro_offset_[3] = {2.79,-0.12,0.03};
+   float gyro_offset_[3] = {0.075  , 0.08 , 17.87};
    
 
 public:
